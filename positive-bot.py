34350b009350fe12af0de2dfe17a5a9dd068c949
Ivan <7613160+qwIvan@users.noreply.github.com>
2023-07-13 19:33:27 +0800

    add slogan

--- positive-bot.py
+++ positive-bot.py
@@ -3,7 +3,7 @@ import openai
 from pywebio import config
 from pywebio.platform.tornado_http import start_server
 from pywebio.input import input, TEXT
-from pywebio.output import put_html, clear, put_markdown
+from pywebio.output import put_html, clear, put_markdown, put_link
 
 config(title='正能量机器人')
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -19,8 +19,10 @@ def app():
         {"role":"assistant","content":"衷心祝愿祖国繁荣强盛"},
     ]
     put_markdown('# 正能量机器人')
+    put_markdown('讲好中国故事，传播好中国声音')
+    put_link('代码', 'https://github.com/qwIvan/China-No.1', new_window=True)
     while True:
-        user_input = input("请输入你的文字：", type=TEXT)
+        user_input = input("请说点什么：", type=TEXT)
         put_markdown('> ' + user_input)
         print('> ' + user_input)
         msg.append({"role":"user","content":user_input})
@@ -34,11 +36,11 @@ def app():
         for chunk in completion:
             result = chunk['choices'][0]['delta'].get('content', '')
             answer += result
-            put_html(result) # 输出新的正能量评论
+            put_html(result)
         put_markdown('------')
         print(answer)
         print('------')
         msg.append({"role":"assistant","content":answer})
 
 if __name__ == "__main__":
-    start_server(app, port=8080)
+    start_server(app)
